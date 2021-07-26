ebad6bb073dd7ade6876dcd3f2969230f6f54441
JImam <jimamtamimi12@gmail.com>
2021-07-26 09:58:12 +0600

    ready for prod

--- core/settings.py
+++ core/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=(zy06n7x=y#&rk4zb22jp^#l(upu5o6r18m=5s1(ernq#@^zx'
+SECRET_KEY = os.environ('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["wsbattendance.herokuapp.com", 'localhost']
 
 
 # Application definition
@@ -165,7 +165,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
 
 
-BASE_URL = "http://127.0.0.1:8000/"
+BASE_URL = os.environ('BASE_URL')
 
 
 
